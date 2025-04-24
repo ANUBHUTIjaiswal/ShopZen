b041c4ca0c7edcc49741c25ef52820db039ebf7a
ANUBHUTIjaiswal <anubhutijaiswal2004@gmail.com>
2025-04-24 16:01:17 +0530

    webpage created (3)

---
+++ src/lib/constants.ts
@@ -0,0 +1,3 @@
+export const APP_NAME = "E-comm";
+export const APP_SLOGAN = "Spend less, enjoy more.";
+export const APP_DESCRIPTION = "Just one click away";
---
+++ src/lib/data.ts
@@ -0,0 +1,69 @@
+const data = {
+  headerMenus: [
+    {
+      name: "Today's Deal",
+      href: "/search?tag=todays-deal",
+    },
+    {
+      name: "New Arrivals",
+      href: "/search?tag=new-arrival",
+    },
+    {
+      name: "Featured Products",
+      href: "/search?tag=featured",
+    },
+    {
+      name: "Best Sellers",
+      href: "/search?tag=best-seller",
+    },
+    {
+      name: "Browsing History",
+      href: "/#browsing-history",
+    },
+    {
+      name: "Customer Service",
+      href: "/page/customer-service",
+    },
+    {
+      name: "About Us",
+      href: "/page/about-us",
+    },
+    {
+      name: "Help",
+      href: "/page/help",
+    },
+  ],
+
+  carousels: [
+    {
+      title: "Most Popular Shoes For Sale",
+      buttonCaption: "Shop Now",
+      image: "/images/banner3.jpg",
+      url: "/search?category=Shoes",
+      isPublished: true,
+    },
+    {
+      title: "Best deals on watches",
+      buttonCaption: "Shop Now",
+      image: "/images/banner2.jpg",
+      url: "/search?category=T-Shirts",
+      isPublished: true,
+    },
+    {
+      title: "Best deals on men collections",
+      buttonCaption: "Shop Now",
+      image: "/images/banner1.jpg",
+      url: "/search?category=T-Shirts",
+      isPublished: true,
+    },
+    {
+      title: "Best deals on women collections",
+      buttonCaption: "Shop Now",
+      image: "/images/banner5.jpg",
+      url: "/search?category=T-Shirts",
+      isPublished: true,
+    },
+  ],
+};
+
+export default data;
